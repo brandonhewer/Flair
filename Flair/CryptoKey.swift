83660e751bb6040c89096ca7e5182320284d2612
brandonhewer <bh532@bath.ac.uk>
2015-11-14 13:35:08 +0000

    q

---
+++ Flair/CryptoKey.swift
@@ -0,0 +1,13 @@
+//
+//  CryptoKey.swift
+//  Flair
+//
+//  Created by Brandon Hewer on 14/11/2015.
+//  Copyright © 2015 Brandon Hewer. All rights reserved.
+//
+
+import UIKit
+
+class CryptoKey {
+    
+}
