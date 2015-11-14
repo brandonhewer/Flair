9830d8b6b68fe517e57097ff0dc856a61ae2d61a
brandonhewer <bh532@bath.ac.uk>
2015-11-14 13:09:04 +0000

    Initial Commit

---
+++ FlairTests/FlairTests.swift
@@ -0,0 +1,36 @@
+//
+//  FlairTests.swift
+//  FlairTests
+//
+//  Created by Brandon Hewer on 14/11/2015.
+//  Copyright © 2015 Brandon Hewer. All rights reserved.
+//
+
+import XCTest
+@testable import Flair
+
+class FlairTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
