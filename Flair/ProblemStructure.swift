45685e8af08bc7c5afde3b8a088431df9c04b549
brandonhewer <bh532@bath.ac.uk>
2015-11-14 13:18:00 +0000

    few

---
+++ Flair/ProblemStructure.swift
@@ -0,0 +1,14 @@
+//
+//  ProblemStructure.swift
+//  Flair
+//
+//  Created by Brandon Hewer on 14/11/2015.
+//  Copyright © 2015 Brandon Hewer. All rights reserved.
+//
+
+import UIKit
+
+class ProblemStructure {
+    
+}
+
